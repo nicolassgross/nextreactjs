b5186e65d2de508282c595fc334392749ccd9e05
nicolassgross <nicolassgross1@gmail.com>
2022-01-26 18:22:27 -0300

    Primeiros exercícios e testes

---
+++ components/Titulo.jsx
@@ -0,0 +1,8 @@
+export default function Titulo(text) {
+    return (
+        <div>
+            <h1>{text.principal}</h1>
+            <h2>{text.secundario}</h2>
+        </div>
+    )
+}
---
+++ pages/basics/usetitulo.jsx
@@ -0,0 +1,17 @@
+import Titulo from '../../components/Titulo'
+
+export default function usandoTitulo() {
+    return(
+        <div>
+            <Titulo 
+                principal="Página de Cadastro"
+                secundario="Incluir, alterar e exlcuir coisas!"
+            />
+
+            <Titulo 
+                principal="Página de login"
+                secundario="Inform seu email e senha"
+            />
+        </div>
+    )
+}
